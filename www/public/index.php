c8e389752598108e046bf4b2d63043768ff09345
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2018-11-15 23:08:22 +0700

    push updated files / 4

--- www/public/index.php
+++ www/public/index.php
@@ -84,7 +84,5 @@ if (!defined('ROOT_PATH')) {
  * Run Application
  * ============================================================ */
 
-include_once dirname(dirname(__FILE__)) . "/vendor/autoload.php";
-
 $application = new Application();
 $application->run();
